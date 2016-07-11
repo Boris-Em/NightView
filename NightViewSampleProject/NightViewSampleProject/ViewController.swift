ae4a8f4c091cd74e9d3f466b6f87c966eccfc411
Boris-Em <boris.emorine@gmail.com>
2016-07-10 19:29:46 -0700

    Add reference to nightView.

--- NightViewSampleProject/NightViewSampleProject/ViewController.swift
+++ NightViewSampleProject/NightViewSampleProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nightView: NightView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
