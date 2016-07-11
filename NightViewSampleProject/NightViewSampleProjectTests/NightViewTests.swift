0a513f7de6a7835eec37ce28daf7c5a5b4fe9caf
Boris-Em <boris.emorine@gmail.com>
2016-07-11 09:34:39 -0700

    Add new NightViewTests file.

---
+++ NightViewSampleProject/NightViewSampleProjectTests/NightViewTests.swift
@@ -0,0 +1,35 @@
+//
+//  NightViewTests.swift
+//  NightViewSampleProject
+//
+//  Created by Bobo on 7/11/16.
+//  Copyright © 2016 Boris Emorine. All rights reserved.
+//
+
+import XCTest
+
+class NightViewTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
